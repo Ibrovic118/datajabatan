2610ce270adced91ffb347404a63e0251699c174
febrians12 <rendif76@gmail.com>
2020-11-09 19:27:07 +0700

    progres kkp

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+      <div class="container">
+        <p class="text-muted">Create By : Luruilmu.com</p>
+      </div>
+    </footer>
+
+
+     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) 
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script> -->
+    <script src="assets/js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.min.js"><script>
+  </body>
+</html>
